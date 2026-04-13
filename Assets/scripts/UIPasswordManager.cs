f9d27cc0c3b52f7acdf3a6aa6e69413c2a0af908
kiwito010 <justorodrip@gmail.com>
2026-04-13 14:36:12 -0300

    tiene errores

---
+++ Assets/scripts/UIPasswordManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class UIPasswordManager : MonoBehaviour
+{
+    public GameObject txt_contraseñaResultado;
+    public TextMeshProUGUI txt_contraseñaResultadoTexto;
+    public TMP_InputField input_Password;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt_contraseñaResultado.SetActive(false);
+    }
+    public void ValidatePassword(){
+         txt_contraseñaResultado.SetActive(true);
+         txt_contraseñaResultadoTexto.text = input_Password.text;
+    }
+}
